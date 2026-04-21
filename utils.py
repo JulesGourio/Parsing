c74390211941b3789c41bf31d5a86f3b9441bd3b
JulesGourio <jules.gourio@etu.inp-n7.fr>
2026-04-21 08:14:17 +0200

    refacto

--- utils.py
+++
@@ -1,8 +0,0 @@
-"""Compatibility layer.
-
-This module now re-exports the parsing toolkit implemented under
-`parsing_core/` to preserve backward compatibility with existing notebook
-imports (`from utils import *`).
-"""
-
-from parsing_core import *
